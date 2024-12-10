c0d4e355b892a8e165fe2dae1606861a841dc3d9
544146 <wee.tommy99@gmail.com>
2024-12-10 13:43:33 +0000

    refactor: remove logging

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 import sqlite3
 from datetime import datetime
 from flask import Flask, request
-import os, tempfile, openai, sys
+import os, tempfile, openai
 from flask.cli import load_dotenv
 from functools import wraps
-from loguru import logger
-from logging import Handler
-
-
-logger.remove()
-logger.add(sys.stderr, format="{time:HH:mm:ss} | {level} | {message}")
-logger.add("app.log", rotation="500 MB", retention="5 days")
 
 load_dotenv()
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-class LoguruHandler(Handler):
-    def emit(self, record):
-        logger.opt(depth=6, exception=record.exc_info)\
-            .log(record.levelname, record.getMessage())
-
-app.logger.handlers = []
-app.logger.addHandler(LoguruHandler())
 
 def init_db(db_path=".notes.db"):
     with sqlite3.connect(db_path) as conn:
@@ -43,14 +27,12 @@ def require_auth_header(f):
     def decorated(*args, **kwargs):
         if request.headers.get(os.getenv("HEADER_KEY")) == os.getenv("HEADER_VALUE"):
             return f(*args, **kwargs)
-        logger.warning(f"Rejected IP: {request.remote_addr}")
         return {}, 403
     return decorated
 
 @app.route("/transcribe", methods=["POST"])
 @require_auth_header
 def transcribe_audio():
-    logger.info("Processing audio transcription")
     with tempfile.NamedTemporaryFile(suffix=".m4a", delete=True) as temp_file:
         temp_file.write(request.data)
         temp_file.flush()
@@ -65,5 +47,4 @@ def transcribe_audio():
 
 if __name__ == "__main__":
     init_db()
-    logger.info("Starting Flask server")
     app.run(debug=bool(os.getenv("DEBUG")), host="0.0.0.0", port=1337)
